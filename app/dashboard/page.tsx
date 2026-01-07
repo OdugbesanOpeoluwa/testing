07e36785f5d2e87a3ee808dd2220208fd8440821
deathwick <odugbesanopeoluwa@gmail.com>
2026-01-07 12:36:56 +0100

    fixed the useEffect in the dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import "useEffect" from "react"
-import "useRouter" from "next/navigation"
+import { useEffect } from "react";
+import { useRouter } from "next/navigation"
 
 export default function Dashboard(){
     const router = useRouter();
 
     useEffect(() => {
-        const token = localStorage.getItems("token");
+        const token = localStorage.getItem("token");
         if(!token){
             router.push("/login");
         }
